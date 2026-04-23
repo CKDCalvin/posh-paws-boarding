1bb176c25147ac7e6be021591ea0579b99c44bf9
CKDCalvin <calvinkugonza@gmail.com>
2026-04-22 22:06:32 -0500

    server file update

--- server.js
+++ server.js
@@ -20,9 +20,23 @@ app.use('/api/reservations', reservationRoutes);
 console.log('PORT:', process.env.PORT);
 
 // Connect to MongoDB
+mongoose.set("bufferCommands", false);
+
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+mongoose.connection.on("error", err => {
+  console.error("Mongoose runtime error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
+
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => console.log('MongoDB connected'))
+// .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
